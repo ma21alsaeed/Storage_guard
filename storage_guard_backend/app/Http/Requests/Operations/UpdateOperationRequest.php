8cef7e47bd68066ed33d0e8890840444fd7812bc
unknown <adnankuza@gmail.com>
2023-05-28 13:40:55 +0300

    init laravel backend project

---
+++ storage_guard_backend/app/Http/Requests/Operations/UpdateOperationRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Operations;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOperationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type'        => 'sometimes|string',
+            'created_at'  => 'sometimes|date',
+            'finished_at' => 'sometimes|date|after:created_at'
+        ];
+    }
+}
---
+++ storage_guard_backend/database/migrations/2023_05_17_151410_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table)
+        {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->timestamp('production_date');
+            $table->timestamp('expity_date');
+            $table->double('max_temp');
+            $table->double('min_temp');
+            $table->double('max_humidity');
+            $table->double('min_humidity');
+            $table->boolean('safety_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
