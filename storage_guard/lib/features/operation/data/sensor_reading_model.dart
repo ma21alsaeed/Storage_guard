8ff1806e7295cec2085b912486e70fcaeb736b74
Adnan <adnankuza@gmail.com>
2023-08-20 22:55:36 +0300

    fixed sensormodel bug

--- storage_guard/lib/features/operation/data/sensor_reading_model.dart
+++ storage_guard/lib/features/operation/data/sensor_reading_model.dart
@@ -4,38 +4,41 @@
 
 import 'dart:convert';
 
-SensorReadingModel sensorReadingModelFromJson(String str) => SensorReadingModel.fromJson(json.decode(str));
+SensorReadingModel sensorReadingModelFromJson(String str) =>
+    SensorReadingModel.fromJson(json.decode(str));
 
-String sensorReadingModelToJson(SensorReadingModel data) => json.encode(data.toJson());
+String sensorReadingModelToJson(SensorReadingModel data) =>
+    json.encode(data.toJson());
 
 class SensorReadingModel {
-    int id;
-    int operationId;
-    int temperature;
-    int humidity;
-    DateTime readAt;
+  int id;
+  int operationId;
+  double temperature;
+  double humidity;
+  DateTime readAt;
 
-    SensorReadingModel({
-        required this.id,
-        required this.operationId,
-        required this.temperature,
-        required this.humidity,
-        required this.readAt,
-    });
+  SensorReadingModel({
+    required this.id,
+    required this.operationId,
+    required this.temperature,
+    required this.humidity,
+    required this.readAt,
+  });
 
-    factory SensorReadingModel.fromJson(Map<String, dynamic> json) => SensorReadingModel(
+  factory SensorReadingModel.fromJson(Map<String, dynamic> json) =>
+      SensorReadingModel(
         id: json["id"],
-        operationId: json["operation_id"],
-        temperature: json["temperature"],
-        humidity: json["humidity"],
+        operationId:json["operation_id"],
+        temperature: double.parse(json["temperature"].toString()),
+        humidity: double.parse(json["humidity"].toString()),
         readAt: DateTime.parse(json["read_at"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "operation_id": operationId,
         "temperature": temperature,
         "humidity": humidity,
         "read_at": readAt.toIso8601String(),
-    };
+      };
 }
